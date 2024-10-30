65be4fb353f778f83db2657fdd21247025b2173d
mercy-rotich <cherotichm182@gmail.com>
2024-10-30 11:44:07 -0700

    made some few changes

--- calc.js
+++ calc.js
@@ -1,30 +1,38 @@
 (function(){
-let screen = document.querySelector('.screen')
-let buttons = document.querySelectorAll('.btn')
-let clear = document.querySelector('.btn-clear')
-let equal  = document.querySelector('.btn-equal');
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function(e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        });
+    });
+
+    
+    equal.addEventListener('click', function(e) {
+        if(screen.value === '') {
+            screen.value = "Enter a value";
+        } else {
+            try {
+                
+                let answer = new Function('return ' + screen.value)();
+                if(answer === Infinity || isNaN(answer)) {
+                    screen.value = "Error";
+                } else {
+                    screen.value = answer;
+                }
+            } catch(error) {
+                screen.value = "Error";
+            }
+        }
+    });
 
 
-
-buttons.forEach(function(button){
-    button.addEventListener('click',function(e){
-        let value=e.target.dataset.num;
-        screen.value +=value;
-    })
-});
-
-
-equal.addEventListener('click',function(e){
-   if(screen.value ==='') {
-    screen.value="Please enter";
-   }else{
-    let answer = eval(screen.value);
-    screen.value=answer;
-   }
-})
-
-clear.addEventListener('click',function(e){
-    screen.value="";
-})
-
-}());
+    clear.addEventListener('click', function(e) {
+        screen.value = "";
+    });
+})();
